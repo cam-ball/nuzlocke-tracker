ec78d4b838ee7c0ac10fa7e46fdf8c5cea8242d8
laurenball <lauren.l.dev@gmail.com>
2021-01-31 22:19:33 -0500

    Load regions, test async

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Pokedex from 'pokedex-promise-v2';
+// import Bootstrap from 'bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      regions: [],
+    }
+  }
+
+  componentDidMount(){
+    this.getRegions()
+  }
+
+  getRegions() {
+    const pokedex = new Pokedex()
+    let promise = pokedex.getRegionsList()
+
+    promise.then(response => {
+      const regions = response.results
+
+      const regionList = regions.map((region, index) => {
+        return (
+          <li key={index}>
+            <button>
+              {region.name}
+            </button>
+          </li>
+        )
+      })
+
+      this.setState({
+        regions: regionList
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="game-info">
+          <ul>{this.state.regions}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
